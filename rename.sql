5430f405084140da83b1038290ded1fa7bc09dd0
Deevashwer <deevashwer@MacBook-Pro.local>
2017-11-12 15:31:35 +0530

    Add rename.sql

--- rename.sql
+++ rename.sql
@@ -1,30 +1,35 @@
-tee /home/deevashwer/Human-Resource-Management-System/output
+alter table contract_based drop foreign key contract_based_ibfk_2;
+alter table contract_based add foreign key (cid) references contracts (cid) on delete cascade on update cascade;
 
-show create table contract_based;
-show create table contract_payment;
-show create table contracts;
-show create table demotion;
-show create table dependents;
-show create table employee;
-show create table employee_account;
-show create table employee_charges;
-show create table employee_loan;
-show create table employee_login;
-show create table employee_phone;
-show create table employee_post;
-show create table initial_pay_scale;
-show create table leave_record;
-show create table loan;
-show create table orders;
-show create table orders_placed;
-show create table pay_scale;
-show create table payment;
-show create table payment_loan;
-show create table post;
-show create table promotion;
-show create table rates;
-show create table regularisation;
-show create table resignation;
-show create table retirement;
-show create table salary;
-show create table ulb;
+
+alter table contract_payment drop foreign key contract_payment_ibfk_2;
+alter table contract_payment add foreign key (cid) references contracts (cid);
+
+
+alter table demotion drop foreign key demotion_ibfk_2;
+alter table demotion add foreign key (lower_pid, lower_uid) references post (pid, uid) on delete no action on update cascade;
+alter table demotion drop foreign key demotion_ibfk_3;
+alter table demotion add foreign key (higher_pid, higher_uid) references post (pid, uid) on delete no action on update cascade;
+
+alter table employee_post drop foreign key employee_post_ibfk_2;
+alter table employee_post add foreign key (pid, uid) references post (pid, uid) on update cascade;
+
+alter table initial_pay_scale drop foreign key initial_pay_scale_ibfk_1;
+alter table initial_pay_scale add foreign key (pid, uid) references post (pid, uid) on delete cascade on update cascade;
+
+alter table orders_placed drop foreign key orders_placed_ibfk_2;
+alter table orders_placed add foreign key (oid) references orders(oid) on delete cascade on update cascade;
+
+alter table payment_loan drop foreign key payment_loan_ibfk_2;
+alter table payment_loan add foreign key (lid) references loan(lid) on delete cascade on update cascade;
+
+alter table post drop foreign key post_ibfk_1;
+alter table post add foreign key (uid) references ulb(uid) on delete cascade on update cascade;
+
+alter table promotion drop foreign key promotion_ibfk_2;
+alter table promotion add foreign key (lower_pid, lower_uid) references post (pid, uid) on delete no action on update cascade;
+alter table promotion drop foreign key promotion_ibfk_3;
+alter table promotion add foreign key (higher_pid, higher_uid) references post (pid, uid) on delete no action on update cascade;
+
+alter table regularisation drop foreign key regularisation_ibfk_2;
+alter table regularisation add foreign key (pid, uid) references post (pid, uid) on delete no action on update cascade;
